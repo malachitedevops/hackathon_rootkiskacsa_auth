e4206b37c308f9e1bd54aa33a615cca770073459
SamuZad <zadori.samu@gmail.com>
2019-09-06 00:57:07 +0200

    Fix(index.js): fixed fetch url

--- view/index.js
+++ view/index.js
@@ -3,7 +3,6 @@
 const button = document.querySelector("button");
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log(querySelectorAll("input")[0].value)
   fetch(`http://localhost:3001/createuser`, {
     method: 'POST',
     mode: "same-origin",
