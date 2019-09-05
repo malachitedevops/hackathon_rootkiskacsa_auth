6dd5eb19acb4b55512e8220fb31d947f831df6da
SamuZad <zadori.samu@gmail.com>
2019-09-06 01:17:55 +0200

    Finished fixing the path

--- view/index.js
+++ view/index.js
@@ -3,17 +3,17 @@
 const button = document.querySelector("button");
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  fetch(`http://localhost:3001/createuser`, {
+  fetch("http://54.81.110.184:3001/createuser", {
     method: 'POST',
-    mode: "no-cors",
+    mode: "same-origin",
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      creatorname: document.querySelectorAll("input")[0].value,
-      creatorpass: document.querySelectorAll("input")[1].value,
-      newusername: document.querySelectorAll("input")[2].value,
-      newuserpass: document.querySelectorAll("input")[3].value,
+      "creatorname": document.querySelectorAll("input")[0].value,
+      "creatorpass": document.querySelectorAll("input")[1].value,
+      "newusername": document.querySelectorAll("input")[2].value,
+      "newuserpass": document.querySelectorAll("input")[3].value,
     }),
   })
 })
