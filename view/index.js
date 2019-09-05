8549f4828a98c6976b9fc4ceae533b67f8035b49
SamuZad <zadori.samu@gmail.com>
2019-09-06 00:12:03 +0200

    Fix(index.js): fixed fetch url

--- view/index.js
+++ view/index.js
@@ -3,7 +3,7 @@
 const button = document.querySelector("button");
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  fetch(`http://ec2-54-81-110-184.compute-1.amazonaws.com/createuser`, {
+  fetch(`http://ec2-54-81-110-184.compute-1.amazonaws.com:3001/createuser`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
