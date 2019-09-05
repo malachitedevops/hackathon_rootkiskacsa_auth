f0888811325178141dc4cfac02e5bf349b90f908
SamuZad <zadori.samu@gmail.com>
2019-09-05 23:48:37 +0200

    Fix(index.js): fixed fetch url

--- view/index.js
+++ view/index.js
@@ -3,17 +3,16 @@
 const button = document.querySelector("button");
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log(document.querySelectorAll('input'));
   fetch(`http://localhost:3001/createuser`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      creatorName: document.querySelectorAll("input")[0].value,
-      creatorPassword: document.querySelectorAll("input")[1].value,
-      newUsername: document.querySelectorAll("input")[2].value,
-      newPassword: document.querySelectorAll("input")[3].value,
+      creatorname: document.querySelectorAll("input")[0].value,
+      creatorpass: document.querySelectorAll("input")[1].value,
+      newusername: document.querySelectorAll("input")[2].value,
+      newuserpass: document.querySelectorAll("input")[3].value,
     }),
   })
 })
