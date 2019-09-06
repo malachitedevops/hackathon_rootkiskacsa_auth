af21d94075a48834032643c5c6cf9b9143d09219
Mark Hegedus <hmark33@gmail.com>
2019-09-06 03:49:05 +0200

    fix(index.js): change fetch path

--- view/index.js
+++ view/index.js
@@ -3,18 +3,17 @@
 const button = document.querySelector("button");
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log(querySelectorAll("input")[0].value)
-  fetch(`http://localhost:3001/createuser`, {
+  fetch("http://3.87.38.201:3001/createuser", {
     method: 'POST',
     mode: "same-origin",
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.parse({
-      creatorname: document.querySelectorAll("input")[0].value,
-      creatorpass: document.querySelectorAll("input")[1].value,
-      newusername: document.querySelectorAll("input")[2].value,
-      newuserpass: document.querySelectorAll("input")[3].value,
+    body: JSON.stringify({
+      "creatorname": document.querySelectorAll("input")[0].value,
+      "creatorpass": document.querySelectorAll("input")[1].value,
+      "newusername": document.querySelectorAll("input")[2].value,
+      "newuserpass": document.querySelectorAll("input")[3].value,
     }),
   })
 })
